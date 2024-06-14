50d075b052fd624d7cd6dc1dc7aef26b24831564
carlosdomke <ca.domke@duocuc.cl>
2024-06-14 13:19:52 -0400

    menu listo

---
+++ ejercicio_prueba.py
@@ -0,0 +1,22 @@
+import os, time 
+
+while True:
+    print("Menú Trabajadores")
+    print("1. Registrar trabajador")
+    print("2. Listar todos los trabajadores")
+    print("3. Imprimir Planilla Sueludo ")
+    print("4. Salir")
+    opc = int(input("Ingrese opción: "))
+    os.system('cls') 
+    if opc == 1:
+        pass
+    elif opc ==2:
+        pass
+    elif opc ==3:
+        pass
+    else:
+        os.system('cls')
+        print("Nos vemos!")
+        break
+    time.sleep(3)
+    
