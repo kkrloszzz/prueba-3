08f2c2145c0627879f1ac03cf73de96f3ccb0dbf
carlosdomke <ca.domke@duocuc.cl>
2024-06-14 14:11:52 -0400

    se crea archivo de funciones (original)

--- ejercicio_prueba.py
+++ ejercicio_prueba.py
@@ -1,8 +1,5 @@
 import os, time 
-
-trabajadores = []
-cargos = ("CEO", "DESARROLLADOR", "ANALISTA")
-
+from funciones import *
 
 while True:
     print("Menú Trabajadores")
@@ -13,15 +10,7 @@ while True:
     opc = int(input("Ingrese opción: "))
     os.system('cls') 
     if opc == 1:
-        print("REGISTRO TRABAJADOR")
-        nombre_apellido = input("Ingrese Nombre Y Apellido: ")
-        cargo = int(input("Ingrese Cargo (1: CEO, 2: DESARROLLADOR, 3: ANALISTA)"))
-        sueldo_bruto = int(input("Ingrese sueldo bruto: "))
-        desc_salud = int(7/100 * sueldo_bruto)
-        desc_afp =  int(12/100 * sueldo_bruto)
-        sueldo_liquido= sueldo_bruto-(desc_salud+desc_afp)
-        trabajador  = [nombre_apellido,cargos[cargo-1],sueldo_bruto,desc_afp,desc_salud,sueldo_liquido]
-        print("TRABAJADOR REGISTRADO!")
+        registrar_trabajador()
 
 
 
--- funciones.py
+++ funciones.py
@@ -1,5 +1,16 @@
+trabajadores = []
+cargos = ("CEO", "DESARROLLADOR", "ANALISTA")
+
 def registrar_trabajador():
-    pass
+    print("REGISTRO TRABAJADOR")
+        nombre_apellido = input("Ingrese Nombre Y Apellido: ")
+        cargo = int(input("Ingrese Cargo (1: CEO, 2: DESARROLLADOR, 3: ANALISTA)"))
+        sueldo_bruto = int(input("Ingrese sueldo bruto: "))
+        desc_salud = int(7/100 * sueldo_bruto)
+        desc_afp =  int(12/100 * sueldo_bruto)
+        sueldo_liquido= sueldo_bruto-(desc_salud+desc_afp)
+        trabajador  = [nombre_apellido,cargos[cargo-1],sueldo_bruto,desc_afp,desc_salud,sueldo_liquido]
+        print("TRABAJADOR REGISTRADO!")
 
 
 def listar_trabajador():
@@ -9,4 +20,5 @@ def exportar_archivo():
     pass
 
 def salir():
-    pass
+    print("GRACIAS ADIOS!")
+    exit()
